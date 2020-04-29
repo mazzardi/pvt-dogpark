46f10f38be3591f789e311cd2be65ab629d0468a
moslehmahamud <mosleh.edu@gmail.com>
2020-04-29 16:14:57 +0200

    added documentation

--- src/main/java/com/pvt/dogpark/controller/JwtAuthenticationController.java
+++ src/main/java/com/pvt/dogpark/controller/JwtAuthenticationController.java
@@ -25,15 +25,34 @@ import lombok.val;
 @CrossOrigin
 public class JwtAuthenticationController {
 
+	/**
+	 * Authentication manager
+	 *
+	 */
 	@Autowired
 	private AuthenticationManager authenticationManager;
 
 	@Autowired
 	private JwtTokenUtil jwtTokenUtil;
 
+	/**
+	 * @varible JwtUserDetailsService
+	 * The reason why userDetailService instance is here
+	 * is becuase to be able to ACCESS that user item
+	 * and CONFIRM
+	 */
 	@Autowired
 	private JwtUserDetailsService userDetailsService;
 
+
+	/**
+	 *
+	 * @param authenticationRequest
+	 * takes in request, that is made in the post
+	 *
+	 * @return The responseEntity(The acutal object essentially)
+	 * @throws Exception
+	 */
 	@PostMapping("/authenticate")
 	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
 
@@ -45,11 +64,26 @@ public class JwtAuthenticationController {
 		return ResponseEntity.ok(new JwtResponse(token));
 	}
 
+	/**
+	 * Gues what it does? xD
+	 * @param user
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public ResponseEntity<?> saveUser(@RequestBody UserDTO user) throws Exception {
 		return ResponseEntity.ok(userDetailsService.save(user));
 	}
 
+	/**
+	 * Authenticates
+	 * @param username
+	 * @param password
+	 * which then sends it to the authentication manager.
+	 * @throws Exception
+	 *
+	 *
+	 */
 	private void authenticate(String username, String password) throws Exception {
 		try {
 			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
