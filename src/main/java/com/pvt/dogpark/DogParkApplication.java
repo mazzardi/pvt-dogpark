3b7971d7ca6f05e05d747cad10d9fb37b7eee1b6
Mattias Rönnberg <ronnberg.mattias@gmail.com>
2020-04-29 17:37:22 +0200

    -imports

--- src/main/java/com/pvt/dogpark/DogParkApplication.java
+++ src/main/java/com/pvt/dogpark/DogParkApplication.java
@@ -1,17 +1,9 @@
 package com.pvt.dogpark;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import com.pvt.dogpark.dto.Quote;
 
 @RestController
 @SpringBootApplication
